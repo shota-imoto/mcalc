1f094c3104ccb7a05dc38ecddcd9c3fec4fe9dee
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-12-30 22:00:21 +0900

    ユーザ登録・認証機能を追加

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,6 @@
 class RootController < ApplicationController
   def index
+    @user = User.new unless user_signed_in?
     @asset_config = AssetConfig.last
     @retirement_asset_calc = RetirementAssetCalc.last
     @yield_config = YieldConfig.last
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'root#index'
 
   resources :asset_formation_calc do
