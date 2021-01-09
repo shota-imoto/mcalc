9a77aee980f67ec3b57876761d7cd13f3eaa15b7
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-01-09 13:52:10 +0900

    メイン設定値の登録、更新ができない不具合が生じていたため修正

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -5,8 +5,10 @@ class RootController < ApplicationController
       @yield_config = current_user.yield_config
       @asset_config = current_user.asset_config
       @retirement_asset_calc = current_user.retirement_asset_calc
-      @asset_formation_calc = AssetFormationCalc.new(@asset_config, @yield_config)
-      @rest_time_calc = RestTimeCalc.new(@asset_formation_calc, @retirement_asset_calc)
+      if @asset_config.present? && @yield_config.present?
+        @asset_formation_calc = AssetFormationCalc.new(@asset_config, @yield_config)
+        @rest_time_calc = RestTimeCalc.new(@asset_formation_calc, @retirement_asset_calc)
+      end
     else
       @user = User.new
     end
