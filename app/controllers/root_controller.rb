975e94b11875da7eabce15d69aa9b4a5da9a3100
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-01-09 21:06:33 +0900

    リタイア期間の計算インスタンス生成のプロセスをBuilderパターンとしてカプセル化することで、コントローラから取り除いた。

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,14 +1,8 @@
 class RootController < ApplicationController
   def index
     if user_signed_in?
-      # todo: implement as builder pattern again.
-      @yield_config = current_user.yield_config
-      @asset_config = current_user.asset_config
-      @retirement_asset_calc = current_user.retirement_asset_calc
-      if @asset_config.present? && @yield_config.present? && @retirement_asset_calc.present?
-        @asset_formation_calc = AssetFormationCalc.new(@asset_config, @yield_config)
-        @rest_time_calc = RestTimeCalc.new(@asset_formation_calc, @retirement_asset_calc)
-      end
+      builder = RestTimeCalcBuilder.new(current_user)
+      @rest_time_calc = builder.rest_time_calc
     else
       @user = User.new
     end
---
+++ app/models/rest_time_calc_builder.rb
@@ -0,0 +1,13 @@
+class RestTimeCalcBuilder
+  attr_accessor :rest_time_calc
+
+  def initialize(user)
+    yield_config = user.yield_config
+    asset_config = user.asset_config
+    retirement_asset_calc = user.retirement_asset_calc
+    if asset_config.present? && yield_config.present? && retirement_asset_calc.present?
+      asset_formation_calc = AssetFormationCalc.new(asset_config, yield_config)
+      @rest_time_calc = RestTimeCalc.new(asset_formation_calc, retirement_asset_calc)
+    end
+  end
+end
