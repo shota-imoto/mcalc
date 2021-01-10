cabe2a8e5be803443d1ab6179208b412241d6dfa
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-01-10 20:32:22 +0900

    エラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def user_confirmed?
+    if user_signed_in?
+      redirect_to new_user_confirmation_path if current_user.confirmed?
+    end
+  end
 end
