38de67d0768723910cfef00edd9508e33ea3ba41
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-04 23:58:08 +0900

    メイン設定APIコントローラをリファクタリング後クラスにあうように変更

--- app/controllers/api/v1/config_controller.rb
+++ app/controllers/api/v1/config_controller.rb
@@ -5,23 +5,14 @@ class Api::V1::ConfigController < ApplicationController
     binding.pry
     if @user
       asset_config = AssetConfig.find_by_user_or_initialize(asset_config_params)
-      yield_config = YieldConfig.find_by_user_or_initialize(yield_config_params)
-
-      # if asset_config.valid? && yield_config.valid?
-        ApplicationRecord.transaction do
-          asset_config.save
-          yield_config.save
-        end
+      if asset_config.save
         config_response = ConfigResponse.new(status: 'success', message: "set configure as #{@user.nickname}", user_id: @user.id)
-      # else
-        #エラー時動作の確認
-        # messages = {}
-        # messages.merge!(asset_config.errors.messages).merge!(yield_config.errors.messages)
-        # config_response = ConfigResponse.new(status: 'error', message: messages, user_id: @user.id)
-      # end
+      else
+        messages = asset_config.errors.messages
+        config_response = ConfigResponse.new(status: 'error', message: messages, user_id: @user.id)
+      end
       serializer = ConfigResponseSerializer.new(config_response)
       render json: serializer.serializable_hash.to_json
-
     else
       message = '認証情報がありません'
       config_response = ConfigResponse.new(status: 'error', message: message)
