345af56c9f6dc6dd3cd5c6bac1439158f16693f9
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-09 21:10:57 +0900

    不要なコントローラ、ルーティングを削除。viewファイルを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include JwtAuthentication
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   def authenticate
     jwt_token = request.authorization&.remove("Token ", "")
     if jwt_token.present?
       payload = decode(jwt_token)
       @user = User.find(payload[0]["sub"])
     end
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
