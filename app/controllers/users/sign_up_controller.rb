6a45bd738553e817a2162b246a0fa86df06aa161
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-14 22:56:34 +0900

    ヘッダーにログイン用のjwtトークンを渡す処理を追加

--- app/controllers/users/sign_up_controller.rb
+++ app/controllers/users/sign_up_controller.rb
@@ -1,7 +1,10 @@
 class Users::SignUpController < ApplicationController
   include UserConfirmation
+
   def index
     if confirm_token
+      jwt_token = issue(params[:user_id])
+      response.headers['X-Authentication-Token'] = jwt_token
       redirect_to "firecountdownapp://home"
     else
       render plain: "不正なアクセス"
