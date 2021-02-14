6f5351ce7478abd2420c0021a76b74d86b72fc2f
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-15 08:32:34 +0900

    リファクタリング

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,5 +1,13 @@
 module MailerHelper
   def url_with_confirmation_token(url, user)
-    url.concat("?user_id=#{user.id}&confirmation_token=#{user.confirmation_token}")
+    additional_url = additional_params(user).inject("?") { |url, (k, v)| url += "#{k}=#{v}&" }
+    url.concat(additional_url)
+  end
+
+  def additional_params(user)
+    {
+      user_id: user.id,
+      confirmation_token: user.confirmation_token
+    }
   end
 end
