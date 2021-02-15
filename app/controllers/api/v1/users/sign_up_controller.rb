f8ba7e6967b01304be9c6b02387aad1348e44d18
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-15 23:17:40 +0900

    不要なhttpレスポンス情報を削除

--- app/controllers/api/v1/users/sign_up_controller.rb
+++ app/controllers/api/v1/users/sign_up_controller.rb
@@ -16,8 +16,6 @@ class Api::V1::Users::SignUpController < ApplicationController
 
   def confirm
     if confirm_token
-      jwt_token = issue(params[:user_id])
-      response.headers['X-Authentication-Token'] = jwt_token
       redirect_to "firecountdownapp://home"
     else
       render plain: "不正なアクセス"
