bb887c743926e37e6eec542381764c7aeb510bcb
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-04-11 19:52:03 +0900

    本番設定

--- config/puma.rb
+++ config/puma.rb
@@ -8,12 +8,15 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+if Rails.env.production?
+  bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+elsif Rails.env.development?
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+
+environment Rails.env
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
